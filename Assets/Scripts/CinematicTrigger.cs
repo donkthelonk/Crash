489b42bb7681cdbdbd3a4a5f68d26e186e9c4738
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-09 23:57:07 -0600

    added CinematicTrigger to scene and asset organization

---
+++ Assets/Scripts/CinematicTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+    }
+}
