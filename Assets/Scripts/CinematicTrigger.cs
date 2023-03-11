470cc95fe0c0e7a7c6f196cc201a250a668f688b
Robert Giles <Robert.ryan.giles@gmail.com>
2023-03-11 15:20:06 -0600

    added code to trigger cutscene when player walks into trigger

--- Assets/Scripts/CinematicTrigger.cs
+++ Assets/Scripts/CinematicTrigger.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class CinematicTrigger : MonoBehaviour
 {
+    [SerializeField] GameObject fpsController;
+    [SerializeField] GameObject cinematicCamera;
+    [SerializeField] GameObject cutscene;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger"); 
+        //Debug.Log("Trigger");
+
+        // disable fps controller
+        fpsController.SetActive(false);
+
+        // enable cutscene object and cinematic camera
+        cinematicCamera.SetActive(true);
+        cutscene.SetActive(true);
     }
 }
